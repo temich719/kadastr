0312b363f142b45e88c36e425ed0bc0fabb22f43
temich719 <temich719@mail.ru>
2025-03-06 02:02:50 +0300

    initial commmit

---
+++ src/main/java/com/example/kadastr/KadastrApplication.java
@@ -0,0 +1,13 @@
+package com.example.kadastr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KadastrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KadastrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kadastr/KadastrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kadastr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KadastrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
